f47e300c3b04ea5afc07962fd39497f7bb8c9227
Ashraf Vali <ads@cubewebsites.com>
2011-12-13 20:56:22 +0000

    Fixed bookmarklet code to load script correctly Ensured that jQuery 1.6.2 is loaded, and set to noconflict mode as soon as it's loaded

--- anti-seo.js
+++ anti-seo.js
@@ -1,31 +1,21 @@
 /*
 * Method to dynamically load jQuery if it's not already on the page
 * Code from http://weblogs.asp.net/joelvarty/archive/2009/05/07/load-jquery-dynamically.aspx
+* Combined with http://net.tutsplus.com/tutorials/javascript-ajax/create-bookmarklets-the-right-way/
 */
-var jQueryScriptOutputted = false;
 function initJQuery() {    
     //if the jQuery object isn't available
     if (typeof(jQuery) == 'undefined') {    
-    
-        if (! jQueryScriptOutputted) {
-            //only output the script once..
-            jQueryScriptOutputted = true;
-            
-            //output the script (load it from google api)
-            document.write("<scr" + "ipt type=\"text/javascript\" src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js\"></scr" + "ipt>");
-        }
-        setTimeout("initJQuery()", 50);
+		script = document.createElement( 'script' );  
+		script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js';  
+		script.onload=initAntiSeo;  
+		document.body.appendChild(script);   
     } else {
-		//jquery now loaded, start the anti-SEO process
-        $(function() {  
-			//set jQuery in no conflict mode
-			$.noConflict();
-            initAntiSeo();
-        });
-    }            
+		initAntiSeo();        
+    }
 }
 initJQuery();
 
 function initAntiSeo() {
-	
+	jQuery.noConflict();	
 }
